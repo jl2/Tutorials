80e762fe0d302dc4f74266a3e681b1bb643032c7
Jeremiah LaRocco <jeremiah.larocco@gmail.com>
2012-06-05 01:36:45 -0600

    Start chapter 3

--- haskell/ch1.hs
+++ haskell/ch1.hs
@@ -11,3 +11,5 @@ boomBang xs = [if x < 10 then "boom" else "bang" | x <- xs, odd x]
 length' xs = sum [ 1 | _ <- xs]
 
 removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+
+rightTriangles = [(a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10], a^2 + b^2 == c^2, a+b+c == 24]
---
+++ haskell/ch3.hs
@@ -0,0 +1,28 @@
+lucky :: (Integral a) => a -> String
+lucky 7 = "Lucky number seven!"
+lucky x = "Sorry, you're out of luck"
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "one"
+sayMe 2 = "two"
+sayMe 3 = "three"
+sayMe 4 = "four"
+sayMe 5 = "five"
+sayMe x = "Not between 1 and 5"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+addVectors :: (Num a) => (a,a) -> (a,a) -> (a,a)
+addVectors (x1, y1) (x2, y2) = (x1+x2, y1+y2)
+
+first :: (a,b,c) -> a
+first (x,_,_)  = x
+
+second :: (a,b,c) -> b
+second (_,y,_) = y
+
+third :: (a,b,c) -> c
+third (_,_,z) = z
+
