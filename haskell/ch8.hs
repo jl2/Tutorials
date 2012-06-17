b83693443795559507d31a17b950a8e10ed2ea89
Jeremiah LaRocco <jeremiah.larocco@gmail.com>
2012-06-16 22:44:34 -0600

    More chapter 8

--- haskell/ch8.hs
+++ haskell/ch8.hs
@@ -2,7 +2,14 @@ module Main (main) where
 
 import Shapes
 
-data Person = Person String String Int Float String String deriving (Show)
+data Person = Person {firstName :: String,
+                      lastName :: String,
+                      age :: Int,
+                      height :: Float,
+                      phoneNumber :: String,
+                      flavor :: String } deriving (Show)
+
+data Car = Car { company :: String, model :: String, year :: Int} deriving (Show)
 
 
 
